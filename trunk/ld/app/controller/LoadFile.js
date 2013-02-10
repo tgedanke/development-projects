22530253ef63e6c2ba2b81e76f51e6216d022fe1
mirrorGML@gmail.com <mirrorGML@gmail.com@8abb0b30-db85-5255-6c89-9159707e1c1d>
2013-02-10 01:14:10 +0000

    загрузка, удаление работают. косяк -на онловд форма не выциплю никак. остальнео пуском

--- trunk/ld/app/controller/LoadFile.js
+++ trunk/ld/app/controller/LoadFile.js
@@ -37,41 +37,62 @@ Ext.define('LFile.controller.LoadFile', {
 	},
 	delFile : function (button)
 	{
-	
-	
 	 var fform = button.up('form');
-	 
-                //if (fform.getForm().isValid()) 
-				{
-				console.log('11111');
-                        fform.submit(
-						//Ext.Ajax.request(
-						{
-                        url: 'data/upload.php',
-						params : {orderNum : '7777', userID : 'webuser', act : 'del'},
-                        waitMsg: 'Удаление...',
-                        success: function(fp, o){
-						console.log('22222');
-						    Ext.Msg.alert('Удалено!', 'Файл ' +o.result.file +" удален");
-							
-							console.log(o.result.file );
-							fform.down('label[name=urlf]').setText('',false);
-							fform.getComponent('itemNumber').setValue('',false);
-							fform.down('button[action=delete]').hide();
-							fform.down('button[action=submit]').show();
-							fform.down('filefield[name=uploadFile]').show();
-							
-					   }
-                    });
-                }
-	
-	
-
-
-	}
+			Ext.Ajax.request(
+			{
+                url: 'data/upload.php',
+				params : {orderNum : '7777', userID : 'webuser', act : 'del'},
+                //waitMsg: 'Удаление...',
+                success: function(fp){
+			jData = Ext.decode(fp.responseText); 
+				//console.log(jData);
+				Ext.Msg.alert('Удалено!', 'Файл ' +jData.file +" удален");
+				//console.log('22222');
+				fform.down('label[name=urlf]').setText('',false);
+				fform.getComponent('itemNumber').setValue('',false);
+				fform.down('button[action=delete]').hide();
+				fform.down('button[action=submit]').show();
+				fform.down('filefield[name=uploadFile]').show();
+			   },
+			   failure : function (response) {
+				Ext.Msg.alert('error!');
+				}
+            });
+ 	}
 
 });
 
 Ext.onReady(function(){
-   Ext.Msg.alert('хидены','призагрузке посмотреть');
+   //Ext.Msg.alert('хидены','призагрузке посмотреть');
+   //aviewport.down('loads');
+   var fform = this.getAdmTool().down('loads');
+ 	// var fform = this.down('form');
+	 console.log(fform);
+			Ext.Ajax.request(
+			{
+                url: 'data/upload.php',
+				params : {orderNum : '7777', userID : 'webuser', act : 'onl'},
+                //waitMsg: 'loading...',
+                success: function(fp){
+			jData = Ext.decode(fp.responseText); 
+				console.log(jData);
+				if (jData.delbtn=="y") {fform.down('button[action=delete]').show();}
+				else {fform.down('button[action=delete]').hide();}
+				//console.log('22222');
+							fform.down('label[name=urlf]').setText( '<a href="data/downloadfile.php?fn=' + jData.dataurl + '"   target="_blank">'+  o.result.file +'</a>',false);
+							fform.getComponent('itemNumber').setValue('<a href="data/downloadfile.php?fn=' + jData.dataurl + '"   target="_blank">'+  o.result.file +'</a>',false);
+							fform.down('button[action=submit]').hide();
+							fform.down('filefield[name=uploadFile]').hide();
+			   },
+			   failure : function (response) {
+				fform.down('label[name=urlf]').setText('',false);
+				fform.getComponent('itemNumber').setValue('',false);
+				fform.down('button[action=delete]').hide();
+				fform.down('button[action=submit]').show();
+				fform.down('filefield[name=uploadFile]').show();
+				
+				}
+            });
+  
+   
 }); 
