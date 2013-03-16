188e12bbc9f07616f52f52dd258dd7cda4f75925
mirrorGML <mirrorGML@gmail.com>
2013-03-16 06:57:11 +0000

    скачивание с реальным именем

--- ld/data/downloadfile.php
+++ ld/data/downloadfile.php
@@ -1,10 +1,28 @@
 <?php
 /*закрузка файла с сервера*/   
 $file = ("tmpfolder/".$_GET['fn']."");
+	$rName='test';
+	$hostname='.'; 
+	$username='dvs'; 
+	$password='';
+	$udatabase='alert_f';
+		$db = mssql_connect($hostname, $username, $password);       
+		mssql_select_db($udatabase);
+	$query = "exec [dbo].sp_select_AgFiles   @RealFileName='{$file}' ";
+	$query = iconv("UTF-8", "windows-1251", $query);
+	$result = mssql_query($query);
+	if (mssql_num_rows($result) > 0)
+		{
+			while ($row = mssql_fetch_array($result, MSSQL_ASSOC)) 			
+			{	
+				$rName = $row["AutorFileName"];
+			}
+		}
+		mssql_close($db);
 header ("Content-Type: application/octet-stream");
 header ("Accept-Ranges: bytes");
 header ("Content-Length: ".filesize($file)); 
-header ("Content-Disposition: attachment; filename=".$file);  
+header ("Content-Disposition: attachment; filename=\"$rName\""); 
 readfile($file);
 
 ?>
